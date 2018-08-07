0125cfdb031c5f45ad147dbb3a875a87e65e2028
Jesse Zhang <sbjesse@gmail.com>
2018-08-06 23:15:12 -0700

    write proper bash

--- dell.bash
+++ dell.bash
@@ -1,6 +1,37 @@
+#!/bin/bash
 # TODO: clang apt source
 # TODO: docker apt source
 
-sudo aptitude install -y vim-gnome fish cmake ninja-build g++ ccache libxerces-c-dev- git parallel clang-tidy-6.0 clang-format-6.0 pigz p7zip-full p7zip-rar rename git-cvs patchelf shellcheck- clang-6.0 lld-6.0 direnv
+_main() {
+	install_packages
+}
+
+install_packages() {
+	local -a packages=(
+	vim-gnome # or vim-gtk2 maybe?
+	fish
+	cmake
+	ninja-build
+	g++
+	ccache
+	libxerces-c-dev- # doesn't work well with orca
+	git
+	parallel
+	clang-tidy-6.0
+	clang-format-6.0
+	pigz
+	p7zip-full
+	p7zip-rar
+	rename
+	patchelf
+	shellcheck- # we want a later-than-apt version
+	clang-6.0
+	lld-6.0
+	direnv
+	)
+	sudo aptitude install -y "${packages[@]}"
+}
 
 # TODO: extract shellcheck probably from Docker Hub?
+
+_main "$@"
